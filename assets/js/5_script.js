890615feaf39fba976a94a781d276a965a9d61f3
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-16 04:31:55 -0500

    creaciones de divs con estilos

--- assets/js/5_script.js
+++ assets/js/5_script.js
@@ -1,33 +1,40 @@
-document.addEventListener('keydown',function (event) {
-    if (event.key === "a") {
-        let div = document.querySelector("div")
-        div.style.backgroundColor="pink"
-        
-    }else if (event.key === "s") {
-        let div = document.querySelector("div")
-        div.style.backgroundColor="orange"
-    }else if (event.key === "d") {
-        let div = document.querySelector("div")
-        div.style.backgroundColor="Skyblue"
-    }else{
-        let div = document.querySelector("div")
-        div.style.backgroundColor="" 
-    }
-    
-})
+document.addEventListener("keydown", function (event) {
+  if (event.key === "a") {
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "pink";
+  } else if (event.key === "s") {
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "orange";
+  } else if (event.key === "d") {
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "Skyblue";
+  } else {
+    let div = document.querySelector("div");
+    div.style.backgroundColor = "";
+  }
+});
 
-
-
-
-
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === "q") {
-        let newdiv = document.createElement('div');
-        const body = document.querySelector("body"); 
-        body.appendChild(newdiv); 
-        newdiv.style.width = "200px";
-        newdiv.style.height = "200px";
-        newdiv.style.backgroundColor = "purple";
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "q") {
+    let newdiv = document.createElement("div");
+    const body = document.querySelector("body");
+    body.appendChild(newdiv);
+    newdiv.style.width = "200px";
+    newdiv.style.height = "200px";
+    newdiv.style.backgroundColor = "purple";
+  } else if (event.key === "w") {
+    let newdiv = document.createElement("div");
+    const body = document.querySelector("body");
+    body.appendChild(newdiv);
+    newdiv.style.width = "200px";
+    newdiv.style.height = "200px";
+    newdiv.style.backgroundColor = "gray";
+  } else if (event.key === "e") {
+    let newdiv = document.createElement("div");
+    const body = document.querySelector("body");
+    body.appendChild(newdiv);
+    newdiv.style.width = "200px";
+    newdiv.style.height = "200px";
+    newdiv.style.backgroundColor = "brown";
+  }
 });
