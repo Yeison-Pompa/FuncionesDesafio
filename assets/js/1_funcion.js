368485ded461c7275bf3ca294231866a490875a5
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-15 23:40:23 -0500

    add archivos js

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,14 @@
+
+// Transformando a una Funcion de expresión
+/* function example(a, b, c,) {
+    return a+b+c
+    
+} */
+ 
+// Funcion de expresion 
+const example = function (a,b,c) {
+    return a+b+c
+    
+}
+
+console.log(example(2,3,5));
