860c81e6394862eb2b992953ef252b901857e580
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-16 00:46:16 -0500

    Cambiando funcion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-function pintar(){
+
+// Funcion anterior
+
+/* function pintar(){
 ele.style.backgroundColor = "yellow"
-}
+} */
 
 const ele = document.querySelector("#ele1");
+const parrafo= document.querySelector("p");
 
-ele.addEventListener("click", pintar)
+/* ele.addEventListener("click", pintar) */
+
+
+// Funcion modificada
+
+ele.addEventListener("click", function() {
+    ele.style.backgroundColor = "green" 
+    parrafo.style.color="yellow"
+})
